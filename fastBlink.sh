eb270101ed5627c15a33657c507f4024aeb52038
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-11-07 16:19:56 -0500

    initial commit

---
+++ fastBlink.sh
@@ -0,0 +1,1 @@
+python /Users/toddflanders/Library/Arduino15/packages/esp32/hardware/esp32/1.0.2/tools/espota.py -i 192.168.0.12 -p 3232 --auth= -f /common/demos/OtaArduino/BasicOTA.fast.bin
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://lastminuteengineers.com/esp32-ota-updates-arduino-ide/
